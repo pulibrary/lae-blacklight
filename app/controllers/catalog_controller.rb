c2c8c6668113aab4c63bd25cb9858b6ad6d57c1a
Jon Stroop <jpstroop@gmail.com>
2015-02-05 11:46:47 -0500

    Changes 'Date Added' to 'Recently Added' and uses date_modified instead

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -180,7 +180,7 @@ class CatalogController < ApplicationController
     # except in the relevancy case).
     config.add_sort_field 'score desc, sort_title asc', label: 'Relevance'
     config.add_sort_field 'sort_title asc', label: 'Title'
-    config.add_sort_field 'date_uploaded asc', label: 'Date Added', show: false
+    config.add_sort_field 'date_modified desc', label: 'Recently Added', show: false
     config.add_sort_field 'date_numsort asc, sort_title asc', label: 'Date Created - Oldest'
     config.add_sort_field 'date_numsort desc, sort_title asc', label: 'Date Created - Newest'
     # If there are more than this many search results, no spelling ("did you
