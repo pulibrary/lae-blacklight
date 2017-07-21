bca901562bb3a98e736434ff83b8232dda0b7fb7
Trey Pendragon <tterrell@princeton.edu>
2017-07-21 13:56:23 -0700

    Load compass earlier. (#154)
    * Load compass earlier.

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'compass'
+require 'breakpoint'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
