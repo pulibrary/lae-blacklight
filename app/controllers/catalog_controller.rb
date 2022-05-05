b96f1e1470c799496c0f2b13b8e500c5da33462c
Trey Pendragon <tpendragon@princeton.edu>
2022-05-05 14:10:12 -0700

    Fix bookmarks.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -21,6 +21,7 @@ class CatalogController < ApplicationController
     config.show.document_actions.delete(:citation)
     config.show.document_actions.delete(:email)
 
+    config.add_results_document_tool(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
     config.add_results_collection_tool(:sort_widget)
     config.add_results_collection_tool(:per_page_widget)
     config.add_results_collection_tool(:view_type_group)
