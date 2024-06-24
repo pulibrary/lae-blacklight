7c3a6daf22fcdeb3b1d213311ac67c312941be7b
Trey Pendragon <tpendragon@princeton.edu>
2024-06-24 10:34:36 -0700

    Don't force TLS.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'lib-ponyexpr-prod.princeton.edu',
-    from: 'no_reply@libruby-prod.princeton.edu'
+    from: 'no_reply@libruby-prod.princeton.edu',
+    enable_starttls: false
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
