a7e03ca72c393a859dd30f94d05bea69269e1a9e
Shaun Ellis <shaun@sdellis.com>
2024-09-12 11:36:56 -0400

    fixes masonry rendering error by updating the masonry config in the catalog controller

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -7,9 +7,7 @@ class CatalogController < ApplicationController
   helper Openseadragon::OpenseadragonHelper
 
   configure_blacklight do |config|
-    # config.view.gallery.partials = [:index_header, :index]
-    config.view.masonry.partials = [:index]
-    # config.view.slideshow.partials = [:index]
+    config.view.masonry(document_component: Blacklight::Gallery::DocumentComponent, icon: Blacklight::Gallery::Icons::MasonryComponent)
     config.index.thumbnail_method = :thumbnail_from_manifest
     # config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
     # config.show.partials.insert(0, :openseadragon)
